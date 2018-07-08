697b2b07d2cfcb374b719ece3c37e5708fe5a485
Michael Fynes <mikefynes@gmail.com>
2018-07-08 12:59:29 +0300

    implementing custom validator using baeldung examples

--- src/main/java/com/fynes/ishoj/models/Person.java
+++ src/main/java/com/fynes/ishoj/models/Person.java
@@ -1,14 +1,38 @@
 package com.fynes.ishoj.models;
 
+import com.fynes.ishoj.validation.ValidEmail;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
+
 public class Person {
 
+    @NotNull
     private int id;
+
+    @NotNull
+    @NotEmpty
     private String name;
 
+    @NotNull
+    @NotEmpty
+    @ValidEmail
+    private String email;
 
-    public Person(int id, String name) {
+
+    public Person(int id, String name, String email) {
         this.id = id;
         this.name = name;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -19,11 +43,27 @@ public class Person {
         this.name = name;
     }
 
-    public int getId() {
-        return id;
+    public String getEmail() {
+        return email;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id &&
+                Objects.equals(name, person.name) &&
+                Objects.equals(email, person.email);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, name, email);
     }
 }
--- src/main/java/com/fynes/ishoj/repositories/PersonRepositoryImpl.java
+++ src/main/java/com/fynes/ishoj/repositories/PersonRepositoryImpl.java
@@ -16,9 +16,9 @@ public class PersonRepositoryImpl implements PersonRepository{
     public PersonRepositoryImpl(){
         // Dummy data for now
         dummyData = new ArrayList<>();
-        dummyData.add(new Person(1,"jim"));
-        dummyData.add(new Person(2,"dave"));
-        dummyData.add(new Person(3,"frank"));
+        dummyData.add(new Person(1,"jim", "jim@example.com"));
+        dummyData.add(new Person(2,"dave", "dave@example.com"));
+        dummyData.add(new Person(3,"frank", "frank@example.com"));
     }
 
     @Override
